3ecb229c87844f7027c8ff206348f7b5f8c2aeae
Neky547 <nekenarab55@gmail.com>
2025-12-04 10:29:22 +0100

    Création class

--- hello.ts
+++ hello.ts
@@ -1,2 +1,8 @@
-let tsMessage = "Hello World !";
-console.log(tsMessage);
+//let tsMessage = "Hello World !";
+//console.log(tsMessage);
+
+class CompteCourant{
+    numCompte: number;
+    solde: number;
+    limiteDecouvert: number;
+}
